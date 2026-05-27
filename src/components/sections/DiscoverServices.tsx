e0b12becdccbe84947a95fcf98eb803044dcebc6
Attharv <attharv.shrivastav@gmail.com>
2026-05-27 13:20:26 +0530

    Service.

--- src/components/sections/DiscoverServices.tsx
+++ src/components/sections/DiscoverServices.tsx
@@ -32,7 +32,7 @@ export const DiscoverServices = () => {
     { name: "Mechanized Road Sweeping", img: "assets/home/services/service-2.webp", fallback: "https://images.unsplash.com/photo-1617112848923-9223a4334b92?q=80&w=2070&auto=format&fit=crop" },
     { name: "Manual Road Sweeping", img: "assets/home/services/service-3.webp", fallback: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=2070&auto=format&fit=crop" },
     { name: "High Pressure Jet Cleaning", img: "assets/home/services/service-4.webp", fallback: "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?q=80&w=2070&auto=format&fit=crop" },
-    { name: "Door to Door Collection & Transport", img: "assets/home/services/service-5.webp", fallback: "https://images.unsplash.com/photo-1530587191325-3db32d826c18?q=80&w=2074&auto=format&fit=crop" },
+    { name: "Door to Door Collection", img: "assets/home/services/service-5.webp", fallback: "https://images.unsplash.com/photo-1530587191325-3db32d826c18?q=80&w=2074&auto=format&fit=crop" },
     { name: "Maintenance of Landscapes", img: "assets/home/services/service-6.webp", fallback: "https://images.unsplash.com/photo-1591193516411-ac56d827aa2d?q=80&w=2070&auto=format&fit=crop" },
     { name: "Bio Remidation", img: "assets/home/services/service-7.webp", fallback: "https://images.unsplash.com/photo-1532375810709-75b1da00537c?q=80&w=2076&auto=format&fit=crop" },
     { name: "Water Rejuvenation", img: "assets/home/services/service-8.webp", fallback: "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=2070&auto=format&fit=crop" },
